765ed1b7302b0898acea70245422227a72f60b8b
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-11 14:53:13 +0200

    update templates

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -18,7 +18,11 @@ urlpatterns = [
     path("create-task/", TaskCreateView.as_view(), name="task-create"),
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("<int:pk>/toggle-is-done", toggle_task_is_done, name="toggle-task-is-done"),
+    path(
+        "<int:pk>/toggle-is-done",
+        toggle_task_is_done,
+        name="toggle-task-is-done"
+    ),
     path(
         "tags/",
         TagListView.as_view(),

b8dc3b8a72831413443f532baa354498d6766b8f
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-11 11:15:24 +0200

    add view and empty template for index page

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from todo_list.models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    context = {
+        "tasks": tasks
+    }
+
+    return render(
+        request,
+        "todo_list/index.html",
+        context=context
+    )
